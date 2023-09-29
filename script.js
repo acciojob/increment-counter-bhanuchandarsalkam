db7095239553d22532134adb0d86a9eea0a9ef2d
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-29 10:53:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var a=0;
+function get(){
+	a=a+1;
+	alert(a);
+	document.getElementById("counter").innerText=a;
+}
+const btn=document.getElementById("incrementBtn");
+btn.addEventListener("click",get);
